e50cd583578debaf41497180df7a6f41dd7945e3
san <stcr96@gmail.com>
2024-02-27 22:45:22 +0900

    Add Header and Footer 2024-02-27

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import "./assets/css/App.css";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div id="App">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
